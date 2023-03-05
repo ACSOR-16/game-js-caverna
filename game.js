b24416641ca0cbcd81069095c95b10dfa3297ecd
oscar <oscar.aguirre.tarazona@gmail.com>
2023-03-04 23:40:35 -0500

    restructuracion de canvas

--- game.js
+++ game.js
@@ -1,22 +1,29 @@
 const canvas = document.querySelector(".game");
 const game = canvas.getContext("2d");
 
+let canvasSize;
+let elementsSize;
+
+window.addEventListener("resize", setCanvasSize)
 window.addEventListener("load", stratGame);
 
+function setCanvasSize() {
+    //Responsive con canvas 
+    if (window.innerHeight > window.innerWidth) {
+      canvasSize = window.innerWidth * 0.8;
+    } else {
+      canvasSize = window.innerHeight * 0.8;
+    }
+  
+    canvas.setAttribute("width", canvasSize);
+    canvas.setAttribute("height", canvasSize);
+  
+    //Tamanio de los elementos de dentro de canvas
+    elementsSize = canvasSize / 10;
+    stratGame();
+}
+
 function stratGame() {
-  //Responsive con canvas 
-  let canvasSize;
-  if (window.innerHeight > window.innerWidth) {
-    canvasSize = window.innerWidth * 0.8;
-  } else {
-    canvasSize = window.innerHeight * 0.8;
-  }
-
-  canvas.setAttribute("width", canvasSize);
-  canvas.setAttribute("height", canvasSize);
-
-  //Tamanio de los elementos de dentro de canvas
-  const elementsSize = canvasSize / 10;
   console.log({canvasSize, elementsSize});
 
   game.font = elementsSize + "px Verdana";
@@ -25,12 +32,4 @@ function stratGame() {
   for (let index = 1; index <= 10; index++) {
     game.fillText(emojis["X"], elementsSize, elementsSize * index);
   }
-  // game.fillRect(0,0,100,100);
-  // game.clearRect(0,0,50,50);
-  // game.clearRect(50,50,100,100);
-
-  // game.font = "25px Verdana";
-  // game.fillStyle = "white";
-  // game.textAlign = "center";
-  // game.fillText("oscar", 50,50)
 }
